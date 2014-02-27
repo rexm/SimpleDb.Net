81aadfa53843def19a594e86fb52056b436d28ed
rexm <rex@rexmorgan.net>
2014-02-27 13:19:05 -0500

    Update PublishNuGetPackage.sh

--- build/PublishNuGetPackage.sh
+++ build/PublishNuGetPackage.sh
@@ -1,4 +1,4 @@
 sed -i "s/<version>.*<\/version>/<version>${BUILD_NUM}<\/version>/g" build/SimpleDb.Net.nuspec
-mono --runtime=v4.0.30319 source/.nuget/NuGet.exe SetApiKey $NUGET_APIKEY
+mono --runtime=v4.0.30319 source/.nuget/NuGet.exe SetApiKey $NUGET_APIKEY -NonInteractive
 mono --runtime=v4.0.30319 source/.nuget/NuGet.exe Pack build/SimpleDb.Net.nuspec
 mono --runtime=v4.0.30319 source/.nuget/NuGet.exe Push build/SimpleDb.Net.nupkg
