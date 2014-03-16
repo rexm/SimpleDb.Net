937c0fe772e1393d5b84f5c952e633730bbe06a6
Rex Morgan <rex@rexmorgan.net>
2014-03-16 18:22:20 -0400

    Update CommitUpdatedAssembly.sh [ci skip]

--- build/CommitUpdatedAssembly.sh
+++ build/CommitUpdatedAssembly.sh
@@ -1,9 +1,3 @@
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "travis-ci"
-git remote set-url origin git@github.com:rexm/SimpleDb.Net.git
-git checkout master
 cp -f source/Cucumber.SimpleDb/bin/Release/Cucumber.SimpleDb.dll .
-echo -e $BUILD_NUM > build/version.txt
-git commit build/version.txt -m "Update version number [ci skip]"
 git commit Cucumber.SimpleDb.dll -m "Update binary to latest ($BUILD_NUM) [ci skip]"
 git push origin master
