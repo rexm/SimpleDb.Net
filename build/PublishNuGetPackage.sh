02450faa13883494d277def4a4e999d5ef34e707
Rex Morgan <rex@rexmorgan.net>
2014-03-23 18:55:56 -0400

    Update PublishNuGetPackage.sh [ci skip]
    Append non-master nuget packages with "-branchname{build#}"

--- build/PublishNuGetPackage.sh
+++ build/PublishNuGetPackage.sh
@@ -1,3 +1,7 @@
-sed -i "s/<version>.*<\/version>/<version>${BUILD_NUM}<\/version>/g" SimpleDb.Net.nuspec
+NUGET_PACKAGE_NUM=$BUILD_NUM
+if [ "$TRAVIS_BRANCH" != "master" ]; then
+	NUGET_PACKAGE_NUM+="-${TRAVIS_BRANCH}${TRAVIS_BUILD_NUMBER}"
+fi
+sed -i "s/<version>.*<\/version>/<version>${NUGET_PACKAGE_NUM}<\/version>/g" SimpleDb.Net.nuspec
 mono --runtime=v4.0.30319 source/.nuget/NuGet.exe Pack SimpleDb.Net.nuspec -NonInteractive
-mono --runtime=v4.0.30319 source/.nuget/NuGet.exe Push SimpleDb.Net.${BUILD_NUM}.nupkg $NUGET_APIKEY -NonInteractive
+mono --runtime=v4.0.30319 source/.nuget/NuGet.exe Push SimpleDb.Net.${NUGET_PACKAGE_NUM}.nupkg $NUGET_APIKEY -NonInteractive
