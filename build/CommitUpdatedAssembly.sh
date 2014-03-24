f1cc58ba4ce42ed09ccbb01ebd51124e77920450
Rex Morgan <rex@rexmorgan.net>
2014-03-23 21:06:56 -0400

    Update CommitUpdatedAssembly.sh

--- build/CommitUpdatedAssembly.sh
+++ build/CommitUpdatedAssembly.sh
@@ -1,3 +1,3 @@
 cp -f source/Cucumber.SimpleDb/bin/Release/Cucumber.SimpleDb.dll .
 git commit Cucumber.SimpleDb.dll -m "Update binary to latest ($BUILD_NUM) [ci skip]"
-git push origin master
+git push origin $TRAVIS_BRANCH
