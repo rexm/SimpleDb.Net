19c40355f7098f9e4e50424ff8afac7294ea4803
Rex Morgan <rex@rexmorgan.net>
2014-04-13 15:06:48 -0400

    Update SetBuildNumber.sh [ci skip]
    Optional argument to increment the build number

--- build/SetBuildNumber.sh
+++ build/SetBuildNumber.sh
@@ -1,13 +1,14 @@
+SHOUD_INCREMENT_BUILD_NUM=$1
 IFS='.'
 read -ra VERS_SEG < "build/version.txt"
-VERS_INCREMENT=${VERS_SEG[${#VERS_SEG[@]}-1]}
-let VERS_INCREMENT+=1
 BUILD_NUM=""
 for((i=0; i<${#VERS_SEG[@]}-1; i++ ));
 do
   BUILD_NUM+="${VERS_SEG[$i]}."
 done
-BUILD_NUM+=$VERS_INCREMENT
+LAST_SEG=${VERS_SEG[${#VERS_SEG[@]}-1]}
+if [ -n "$SHOULD_INCREMENT_BUILD_NUM"  ]; then let LAST_SEG+=1 fi
+BUILD_NUM+=$LAST_SEG
 echo "Build number: $BUILD_NUM"
 export BUILD_NUM
 unset IFS
